db814fca29f85ae5637b76b98200e96005e8b8b3
Afif Firmansyah <Afif_173040115@mail.unpas.ac.id>
2020-01-02 16:20:09 +0700

    menambahkan struktur folder

---
+++ src/com/synesthesia/laundryku/main/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.synesthesia.laundryku.main;
+
+import com.synesthesia.laundryku.view.LoginFrame;
+
+/**
+ *
+ * @author AFIF FIRMANSYAH
+ */
+public class Main {
+        public static void main(String[] args) {
+        LoginFrame l = new LoginFrame();
+        l.setVisible(true);
+    }
+}
